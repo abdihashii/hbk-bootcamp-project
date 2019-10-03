b6e367f5145d306131fe07978454241db2945b34
haji <abdirahman.haji.13@gmail.com>
2019-10-03 15:49:31 -0700

    renmaing route functions for Vote and Home

--- voting-tool/src/components/common/home.js
+++ voting-tool/src/components/common/home.js
@@ -4,14 +4,14 @@ import Button from '@material-ui/core/Button';
 
 const Home = () => {
 
-  const openVote = () => window.location.href = '/#/vote';
+  const routeToVote = () => window.location.href = '/#/vote';
 
   return (
     <div>
       <div>
         Home Page
       </div>
-        <Button variant="contained" color="primary" onClick={openVote}>Vote</Button>
+        <Button variant="contained" color="primary" onClick={routeToVote}>Vote</Button>
     </div>
   );
 };
