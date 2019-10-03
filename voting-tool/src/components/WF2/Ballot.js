0000b205022e94a428e72a904a747b350db9910e
haji <abdirahman.haji.13@gmail.com>
2019-10-03 15:47:42 -0700

    Adding checkbox to Ballot UI - states are definied

--- voting-tool/src/components/WF2/Ballot.js
+++ voting-tool/src/components/WF2/Ballot.js
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Ballot = ({elections}) => {
+import { Checkbox, Button } from '@material-ui/core';
+
+const Ballot = ({currentElection}) => {
+
+  // const [ isChecked, setChecked ] = useState(false);
+
+  const [ isChecked, setChecked ] = useState({});
+
+  const onToggleChecked = (questionId) => {
+    setChecked({
+      ...isChecked,
+      [questionId]: !isChecked[questionId]
+    })
+  };
+  
+  // const toggleCheck = index => {
+  //   if (isChecked === false) {
+  //     console.log(`${index} is checked`);
+  //   }
+  //   setChecked(!isChecked);
+  // };
+
+  // const onToggleCheck = (index) => {
+  //   // console.log(`Checked`);
+  //   setChecked(isChecked[index] = !isChecked[index]);
+  // }
+  
+  // elections[0].questions.forEach((i) => {
+  //   setChecked([...isChecked[i], false])
+  // });
 
   return (
     <>
       <div>Ballot</div>
       <div>
-        <p>Q1:</p>
-        <p>{elections[0].questions[0]}</p>
-        <input type="checkbox"></input>
-        <p>Q2:</p>
-        <p>{elections[0].questions[1]}</p>
-        <input type="checkbox"></input>
-        <p>Q3:</p>
-        <p>{elections[0].questions[2]}</p>
-        <input type="checkbox"></input>
+        <form>
+
+
+          {/* {currentElection.questions.map((key, index) => { */}
+          {currentElection.questions.map((question) => {
+            return (
+              <div key={question.id}>
+                {/* {console.log(question.text)} */}
+                <p>Q{question.id}</p>
+                <p>{question.text}</p>
+                <Checkbox checked={isChecked[question.id] || false} type="checkbox" name={`question-${question.id}`} color="secondary" onChange={() => onToggleChecked(question.id)}></Checkbox>
+              </div>
+            );
+          })}
+
+          <div>
+            <Button variant="contained" color="primary">Cast Vote</Button>
+          </div>
+        </form>
       </div>
     </>
   );
