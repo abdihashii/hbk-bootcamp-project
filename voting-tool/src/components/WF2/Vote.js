3c64ee130c20c5d424084e9aa0e117c2b6924b9d
haji <abdirahman.haji.13@gmail.com>
2019-10-03 11:43:38 -0700

    Vote page has button that links to ballot page

--- voting-tool/src/components/WF2/Vote.js
+++ voting-tool/src/components/WF2/Vote.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 const Vote = () => {
+
+  const beginBallot = () => {
+
+    window.location.href = '/#/vote/ballot';
+    
+  };
+
   return (
-    <div>Voting Page</div>
+    <div>
+      <div>Voting Page</div>
+      <div>
+        <form>
+          <label htmlFor="voter-valid-input">Identifying Information</label>
+          <div>
+            <input type="number" id="voter-valid-input" name="voterId" />
+          </div>
+          <div>
+            <button type="button" onClick={beginBallot}>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
