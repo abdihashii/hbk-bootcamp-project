2478d3ac0e8d8dfc8b6a6feb4675d3b720160885
Ken <Kenneth_Bigler@intuit.com>
2019-10-03 10:52:34 -0700

    pushing material ui config

--- voting-tool/src/index.js
+++ voting-tool/src/index.js
@@ -4,12 +4,25 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { configureStore } from './redux/configureStore';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 const store = configureStore();
 
+const setTheme = createMuiTheme({
+  typography: { useNextVariants: true },
+  palette: {
+    primary: { main: '#E39B48' },
+    secondary: { main: '#FFBED6' },
+  },
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={setTheme}>
+      <CssBaseline />
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
